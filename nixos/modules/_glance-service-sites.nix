69b883df0cf390a2be3d0b9a24f8aa1d99cbb191
yz <260740417+yzua@users.noreply.github.com>
2026-03-04 22:37:53 +0300

    refactor(glance): extract service sites and YouTube channels
    Move data lists to helper files for cleaner main module:
    - _glance-service-sites.nix: local service monitor URLs
    - _glance-youtube-channels.nix: video feed subscriptions

---
+++ nixos/modules/_glance-service-sites.nix
@@ -0,0 +1,47 @@
+[
+  {
+    title = "Glance";
+    url = "http://localhost:8082";
+    icon = "mdi:view-dashboard-outline";
+  }
+  {
+    title = "Netdata";
+    url = "http://localhost:19999";
+    icon = "si:netdata";
+  }
+  {
+    title = "Grafana";
+    url = "http://localhost:3001";
+    icon = "si:grafana";
+  }
+  {
+    title = "Prometheus";
+    url = "http://localhost:9090";
+    icon = "si:prometheus";
+  }
+  {
+    title = "Alertmanager";
+    url = "http://localhost:9093";
+    icon = "mdi:alert-outline";
+  }
+  {
+    title = "Scrutiny";
+    url = "http://localhost:8080";
+    icon = "mdi:harddisk";
+  }
+  {
+    title = "I2PD Webconsole";
+    url = "http://localhost:7070";
+    icon = "mdi:router-network";
+  }
+  {
+    title = "Syncthing";
+    url = "http://localhost:8384";
+    icon = "si:syncthing";
+  }
+  {
+    title = "ActivityWatch";
+    url = "http://localhost:5600";
+    icon = "mdi:clock-check-outline";
+  }
+]
--- nixos/modules/glance.nix
+++ nixos/modules/glance.nix
@@ -117,53 +117,7 @@
                     type = "monitor";
                     title = "Services";
                     cache = "1m";
-                    sites = [
-                      {
-                        title = "Glance";
-                        url = "http://localhost:8082";
-                        icon = "mdi:view-dashboard-outline";
-                      }
-                      {
-                        title = "Netdata";
-                        url = "http://localhost:19999";
-                        icon = "si:netdata";
-                      }
-                      {
-                        title = "Grafana";
-                        url = "http://localhost:3001";
-                        icon = "si:grafana";
-                      }
-                      {
-                        title = "Prometheus";
-                        url = "http://localhost:9090";
-                        icon = "si:prometheus";
-                      }
-                      {
-                        title = "Alertmanager";
-                        url = "http://localhost:9093";
-                        icon = "mdi:alert-outline";
-                      }
-                      {
-                        title = "Scrutiny";
-                        url = "http://localhost:8080";
-                        icon = "mdi:harddisk";
-                      }
-                      {
-                        title = "I2PD Webconsole";
-                        url = "http://localhost:7070";
-                        icon = "mdi:router-network";
-                      }
-                      {
-                        title = "Syncthing";
-                        url = "http://localhost:8384";
-                        icon = "si:syncthing";
-                      }
-                      {
-                        title = "ActivityWatch";
-                        url = "http://localhost:5600";
-                        icon = "mdi:clock-check-outline";
-                      }
-                    ];
+                    sites = import ./_glance-service-sites.nix;
                   }
 
                   # NOTE: Tor exposes SOCKS/DNS ports (9050/9053), not an HTTP UI endpoint,
@@ -194,46 +148,7 @@
                     type = "videos";
                     title = "YouTube";
                     style = "grid-cards";
-                    channels = [
-                      # Fireship
-                      "UCsBjURrPoezykLs9EqgamOA"
-                      # ThePrimeagen
-                      "UCUyeluBRhGPCW4rPe_UvBZQ"
-                      # No Boilerplate
-                      "UCc0YbtMkRdhcqwhu3Oad-lw"
-                      # Beyond Fireship
-                      "UC2Xd-TjJByJyK2w1zNwY0zQ"
-                      # Better Stack
-                      "UCkVfrGwV-iG9bSsgCbrNPxQ"
-                      # codingjerk
-                      "UCFPTbsXLqWLHcXosYYw3D6Q"
-                      # PwnFunction
-                      "UCW6MNdOsqv2E9AjQkv9we7A"
-                      # ByteByteGo
-                      "UCZgt6AzoyjslHTC9dz0UoTw"
-                      # Dreams of Code
-                      "UCWQaM7SpSECp9FELz-cHzuQ"
-                      # Seytonic
-                      "UCW6xlqxSY3gGur4PkGPEUeA"
-                      # Bubble Brian
-                      "UCvF3C7NCZBHuE9iJvmsxO3w"
-                      # bigboxSWE
-                      "UC5--wS0Ljbin1TjWQX6eafA"
-                      # Code to the Moon
-                      "UCjREVt2ZJU8ql-NC9Gu-TJw"
-                      # Mental Outlaw
-                      "UC7YOGHUfC1Tb6E4pudI9STA"
-                      # Vimjoyer
-                      "UC_zBdZ0_H_jn41FDRG7q4Tw"
-                      # dacctal
-                      "UCTMiGZ6t4wry5sk7Cso2plQ"
-                      # Theo - t3.gg
-                      "UCbRP3c757lWg9M-U7TyEkXA"
-                      # Dylan Page
-                      "UCzPpbeK8ANcNKg5aoMB0miw"
-                      # Awesome
-                      "UCXzw-OdotBUcNA9yhuYQBwA"
-                    ];
+                    channels = import ./_glance-youtube-channels.nix;
                   }
                 ];
               }
