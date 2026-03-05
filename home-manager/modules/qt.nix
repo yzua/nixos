1b5e20e1f1550da1128e7e2527eef1e99cb84acb
yz <260740417+yzua@users.noreply.github.com>
2026-03-05 21:58:39 +0300

    feat(qt): add qt5ct/qt6ct settings with Gruvbox theme
    Configure Qt theme settings explicitly for qtct platform:
    - Set fusion style with Gruvbox-Plus-Dark icon theme
    - Configure fixed and general fonts from constants

--- home-manager/modules/qt.nix
+++ home-manager/modules/qt.nix
@@ -1,11 +1,40 @@
 # Qt theming (Kvantum + Gruvbox).
-{ lib, pkgs, ... }:
+{
+  constants,
+  lib,
+  pkgs,
+  ...
+}:
 
 {
   qt = {
     enable = true;
     platformTheme.name = "qtct";
     style.name = lib.mkForce "fusion";
+    qt5ctSettings = {
+      Appearance = {
+        custom_palette = true;
+        icon_theme = "Gruvbox-Plus-Dark";
+        standard_dialogs = "default";
+        style = "fusion";
+      };
+      Fonts = {
+        fixed = "\"${constants.font.mono},11\"";
+        general = "\"Noto Sans,11\"";
+      };
+    };
+    qt6ctSettings = {
+      Appearance = {
+        custom_palette = true;
+        icon_theme = "Gruvbox-Plus-Dark";
+        standard_dialogs = "default";
+        style = "fusion";
+      };
+      Fonts = {
+        fixed = "\"${constants.font.mono},11\"";
+        general = "\"Noto Sans,11\"";
+      };
+    };
   };
 
   home.packages = with pkgs; [
