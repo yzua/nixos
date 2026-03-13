92af3ae187f9651ce32e1d66bfaf31615c2e6665
yz <260740417+yzua@users.noreply.github.com>
2026-03-13 17:04:23 +0300

    style(terminal): expand fzf Gruvbox color theming
    Add full Gruvbox dark soft theming with all available color options:
    - Text colors (fg, bg, hl)
    - Selected item colors (fg+, bg+, hl+)
    - UI element colors (info, prompt, pointer, marker, spinner, header)
    - Border and gutter colors
    
    Matches the broader Gruvbox theme used across the system.

--- home-manager/modules/terminal/tools/fzf.nix
+++ home-manager/modules/terminal/tools/fzf.nix
@@ -1,4 +1,4 @@
-# Fzf configuration with Gruvbox theming.
+# Fzf configuration with full Gruvbox dark soft theming.
 
 { constants, ... }:
 
@@ -12,12 +12,24 @@
       "--border"
     ];
     colors = {
+      # Text colors
       "fg" = constants.color.fg0;
-      "bg" = constants.color.bg;
+      "bg" = constants.color.bg_soft;
       "hl" = constants.color.yellow;
+      # Selected item
       "fg+" = constants.color.fg0;
       "bg+" = constants.color.bg0;
       "hl+" = constants.color.yellow;
+      # UI elements
+      "info" = constants.color.aqua;
+      "prompt" = constants.color.blue;
+      "pointer" = constants.color.orange;
+      "marker" = constants.color.green;
+      "spinner" = constants.color.purple;
+      "header" = constants.color.gray;
+      # Border and gutter
+      "border" = constants.color.bg1;
+      "gutter" = constants.color.bg;
     };
   };
 }
