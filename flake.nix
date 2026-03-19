5902b5f4c37fa93fee6464976b4716c2d34e8549
yz <260740417+yzua@users.noreply.github.com>
2026-03-19 18:09:13 +0300

    chore(deps): update flake inputs and pin sops-nix revision

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
     };
 
     sops-nix = {
-      url = "github:Mic92/sops-nix";
+      url = "github:Mic92/sops-nix/17eea6f3816ba6568b8c81db8a4e6ca438b30b7c";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
