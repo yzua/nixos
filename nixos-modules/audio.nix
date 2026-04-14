794d5cbef1aca70eec84714a7b00ba3c5851d3a0
yz <260740417+yzua@users.noreply.github.com>
2026-04-14 21:05:10 +0300

    refactor(audio): move Bluetooth codec config to audio.nix
    Bluetooth audio codecs (aptX, LDAC, AAC, SBC-XQ) are audio concerns.
    Move from bluetooth.nix to audio.nix, guarded by
    config.mySystem.bluetooth.enable.

--- nixos-modules/audio.nix
+++ nixos-modules/audio.nix
@@ -1,6 +1,12 @@
-# PipeWire audio stack (ALSA, PulseAudio compat, JACK, RNNoise).
+# PipeWire audio stack (ALSA, PulseAudio compat, JACK, RNNoise, Bluetooth codecs).
 
-{ pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  pkgsStable,
+  ...
+}:
 
 let
   rnnoise = pkgs.rnnoise-plugin;
@@ -17,6 +23,25 @@ in
       alsa.support32Bit = true;
       pulse.enable = true;
       jack.enable = true;
+
+      # High-quality Bluetooth audio codecs (aptX, aptX-HD, LDAC, AAC, SBC-XQ)
+      wireplumber.extraConfig = lib.mkIf config.mySystem.bluetooth.enable {
+        "10-bluez" = {
+          "monitor.bluez.properties" = {
+            "bluez5.enable-sbc-xq" = true;
+            "bluez5.enable-msbc" = true;
+            "bluez5.enable-hw-volume" = true;
+            "bluez5.codecs" = [
+              "sbc"
+              "sbc_xq"
+              "aac"
+              "ldac"
+              "aptx"
+              "aptx_hd"
+            ];
+          };
+        };
+      };
 
       extraConfig.pipewire = {
         # Increase buffer size to reduce audio stuttering in Electron apps (pear-desktop).
@@ -65,5 +90,8 @@ in
     };
   };
 
-  environment.systemPackages = [ pkgs.rnnoise-plugin ];
+  environment.systemPackages = [
+    pkgs.rnnoise-plugin
+  ]
+  ++ lib.optionals config.mySystem.bluetooth.enable [ pkgsStable.libfreeaptx ];
 }
